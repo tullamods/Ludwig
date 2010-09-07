3f62871741333ef4bc8bb550b30083f4f475eea0
Tuller <j85ason+git@gmail.com>
2010-09-07 18:53:25 -0400

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ rm -rf "${WOW_BETA_ADDON_DIR}Ludwig"
 cp -r Ludwig "${WOW_BETA_ADDON_DIR}"
 
 cp LICENSE "${WOW_BETA_ADDON_DIR}Ludwig"
-cp README  "${WOW_BETA_ADDON_DIR}Ludwig"
+cp README.textile  "${WOW_BETA_ADDON_DIR}Ludwig"
