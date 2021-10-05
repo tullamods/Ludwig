c565edc8cb52a87e05efa981746abe11880e9261
Adavak <adavak@adavak.com>
2021-10-05 11:32:24 +0800

    Update cn.lua

--- addons/main/locales/cn.lua
+++ addons/main/locales/cn.lua
@@ -7,6 +7,10 @@ if GetLocale() ~= "zhCN" then return end
 local ADDON, Addon = ...
 local L = Addon('Locals')
 
+--Keybinding text
+BINDING_HEADER_LUDWIG = "Ludwig"
+BINDING_NAME_LUDWIG_SHOW = "显示 Ludwig"
+
 --UI text
 L.FrameTitle = "Ludwig: %d件物品"
 
@@ -21,6 +25,7 @@ L.TradeGoods = "商品"
 
 --Slash command responses
 L.UnknownCommand = "\"%s\"命令不能被识别"
+L.MissingNumber = "缺少要查询的物品 ID"
 L.NoMatchingItems = "没有物品能够匹配关键字\"%s\""
 L.NumMatching = "共有%d件物品能够匹配关键字\"%s\"："
 L.GenTime = "耗时%.3f秒"
